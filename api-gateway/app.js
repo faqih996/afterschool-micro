2e108b9ea19fc22a5686bfaa83cac2939a1c5da3
Faqih996 <faqih.syakir11@gmail.com>
2023-04-11 19:09:20 +0700

    create API router & Install axios

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -1,14 +1,18 @@
 require('dotenv').config();
 
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const coursesRouter = require('./routes/courses');
+const mediaRouter = require('./routes/media');
+const ordersRouter = require('./routes/orders');
+const paymentsRouter = require('./routes/payments');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -18,5 +22,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/courses', coursesRouter);
+app.use('/orders', ordersRouter);
+app.use('/media', mediaRouter);
+app.use('/payments', paymentsRouter);
 
 module.exports = app;
--- api-gateway/routes/courses.js
+++ api-gateway/routes/courses.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const { APP_NAME } = process.env;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('users');
+  res.send('courses');
 });
 
 module.exports = router;
