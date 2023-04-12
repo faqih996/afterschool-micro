f17130930c5182ac1d7d9c962aae52d086b1f372
Faqih996 <faqih.syakir11@gmail.com>
2023-04-12 15:29:04 +0700

    api-upload-base64

--- service-media/routes/media.js
+++ service-media/routes/media.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const isBase64 = require('is-base64');
+const base64Img = require('base64-img');
 
 router.post('/', (req, res) => {
-  res.send('test 12345')
+  const image = req.body.image;
+
+  if (!isBase64(image, { mimeRequired: true })) {
+    return res.status(400).json({ status: 'error', message: 'invalid base64' });
+  }
+
+  return res.send('ok');
 });
 
 module.exports = router;
