1585e90e9ddd92c83f2b1b99f100e0fb585797fb
Faqih996 <faqih.syakir11@gmail.com>
2023-04-16 15:53:44 +0700

    create-model-user-and-refresh-token

--- service-user/seeders/20230416080403-user-seeders.js
+++ service-user/seeders/20230416080403-user-seeders.js
@@ -1,25 +1,32 @@
 'use strict';
+const bycript = require('bcrypt');
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+      
+    await queryInterface.bulkInsert('users', [
+      {
+        name: 'Starter',
+        profession: "Admin Micro",
+        role: "admin",
+        email: "faqih.syakir11@gmail.com",
+        password: await bycript.hash('faqih996', 10),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Santuy',
+        profession: "Back End Developher",
+        role: "student",
+        email: "user@mail.com",
+        password: await bycript.hash('syakir996', 10),
+        created_at: new Date(),
+        updated_at: new Date(),
+      }
+    ]);
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('users', null, {});
   }
 };
