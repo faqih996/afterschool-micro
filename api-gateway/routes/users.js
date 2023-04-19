e3d7d17912acccd4d4734c5e7d01fffea81821de
Faqih996 <faqih.syakir11@gmail.com>
2023-04-19 11:21:27 +0700

    set up API gateway and JWT

--- api-gateway/routes/users.js
+++ api-gateway/routes/users.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
-const { APP_NAME } = process.env;
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('users');
-});
+const express = require('express');
+const router = express.Router();
+
+const usersHandler = require('./handler/users');
+
+router.post('/register', usersHandler.register);
 
 module.exports = router;
