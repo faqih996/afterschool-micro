e71144544b56b845ac2cf5b6cae157d9f391c7d7
Faqih996 <faqih.syakir11@gmail.com>
2023-04-24 16:15:51 +0700

    My Course API

---
+++ service-course/app/Http/Controllers/ReviewController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Course;
+use App\Models\MyCourse;
+use App\Models\Review;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ReviewController extends Controller
+{
+}
