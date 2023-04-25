8a4013f583557bab8a13c1f804766e4ffc20a503
Faqih996 <faqih.syakir11@gmail.com>
2023-04-25 15:38:30 +0700

    API Gateway Courses

--- api-gateway/routes/courses.js
+++ api-gateway/routes/courses.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
-const { APP_NAME } = process.env;
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('courses');
-});
+
+const express = require('express');
+const router = express.Router();
+
+const verifyToken = require('../middlewares/verifyToken');
+
+const coursesHandler = require('./handler/courses');
+
+
+router.get('/', coursesHandler.getAll);
+router.get('/:id', coursesHandler.get);
+
+
+router.post('/', verifyToken, coursesHandler.create);
+router.put('/:id', verifyToken, coursesHandler.update);
+router.delete('/:id', verifyToken, coursesHandler.destroy);
 
 module.exports = router;
