afc124ea310e5ca0806c4ab92ce93f2693a1a93f
Faqih996 <faqih.syakir11@gmail.com>
2023-05-01 22:09:11 +0700

    Create Order Finish

--- service-order-payment/app/Http/Controllers/OrderController.php
+++ service-order-payment/app/Http/Controllers/OrderController.php
@@ -2,11 +2,72 @@
 
 namespace App\Http\Controllers;
 
-use app\Models\Order;
+use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class OrderController extends Controller
 {
+
+    public function create(Request $request)
+    {
+        $user = $request->input('user');
+        $course = $request->input('course');
+
+        $order = Order::create([
+            'user_id' => $user['id'],
+            'course_id' => $course['id']
+        ]);
+
+        $transactionDetails = [
+            // to make order id + random string to midtrans
+            'order_id' => $order->id . Str::random(5),
+            'gross_amount' => $course['price']
+        ];
+
+        $itemDetails = [
+            [
+                'id' => $course['id'],
+                'price' => $course['price'],
+                'quantity' => 1,
+                'name' => $course['name'],
+                'brand' => 'Sea Education',
+                'category' => 'Training'
+            ]
+        ];
+
+        $customerDetails = [
+            'first_name' => $user['name'],
+            'email' => $user['email']
+        ];
+
+        // to set parameter snap url midtrans
+        $midtransParams = [
+            'transaction_details' => $transactionDetails,
+            'item_details' => $itemDetails,
+            'customer_details' => $customerDetails
+        ];
+
+        //  to get snap url midtrans
+        $midtransSnapUrl = $this->getMidtransSnapUrl($midtransParams);
+
+        $order->snap_url = $midtransSnapUrl;
+
+        $order->metadata = [
+            'course_id' => $course['id'],
+            'course_price' => $course['price'],
+            'course_name' => $course['thumbnail'],
+            'course_level' => $course['level']
+        ];
+
+        $order->save();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $order
+        ]);
+    }
+
     private function getMidtransSnapUrl($params)
     {
         \Midtrans\Config::$serverKey = env('MIDTRANS_SERVER_KEY');
